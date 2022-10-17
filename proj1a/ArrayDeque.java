74485768c56e67457931a3f54bc3d4c3f2ce05c0
yeyy <211220155@smail.nju.edu.cn>
2022-10-17 21:18:33 +0800

    proj1a_2

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,7 +1,7 @@
 public class ArrayDeque<T>{
-    T []items;
-    int size;
-    int front,rear;
+    private T []items;
+    private int size;
+    private int front,rear;
     /*invariant: front points to the position before the first item;
     rear points to the last item;
     if rear==front, the deque is empty.
@@ -11,14 +11,16 @@ public class ArrayDeque<T>{
         size=0;
         front=rear=0;
     }
-    public void resize(int capacity) {
+    private void resize(int capacity) {
         T[] temp = (T[]) new Object[capacity];
         if(front<=rear) {
             System.arraycopy(items, front + 1, temp, 0, size);
+            front=capacity-1;
+            rear=size-1;
         }else{
             System.arraycopy(items,0,temp,0,rear+1);
-            System.arraycopy(items,front+1,temp,capacity-size+front,size-front-1);
-            front=capacity-size+front-1;
+            System.arraycopy(items,front+1,temp,capacity-items.length+front+1,size-rear-1);
+            front=capacity-items.length+front;
         }
         items = temp;
     }
@@ -26,13 +28,13 @@ public class ArrayDeque<T>{
         return (front+1)%items.length;
     }
     private int decFront(){
-        return (front-1)%items.length;
+        return (front-1+items.length)%items.length;
     }
     private int incRear(){
         return (rear+1)%items.length;
     }
     private int decRear(){
-        return (rear-1)%items.length;
+        return (rear-1+items.length)%items.length;
     }
     private boolean isFull(){
         return incRear()==front;
@@ -54,11 +56,23 @@ public class ArrayDeque<T>{
         size++;
     }
     public T removeFirst(){
+        if(size==0){
+            return null;
+        }
+        if(size/items.length<0.25){//usage<0.25
+            resize(2*size);
+        }
         size--;
         front=incFront();
         return items[front];
     }
     public T removeLast(){
+        if(size==0){
+            return null;
+        }
+        if(size/items.length<0.25){//usage<0.25
+            resize(2*size);
+        }
         size--;
         T val=items[rear];
         rear=decRear();
@@ -71,9 +85,35 @@ public class ArrayDeque<T>{
         if(index>=size){
             return null;
         }
-        return items[index];
+        return items[index+front+1];
     }
     public boolean isEmpty(){
         return size==0;
     }
+    public void printDeque(){
+        int cur=front+1;
+        if(front==rear){
+            return;
+        }
+        while(cur!=rear){
+
+            System.out.print(items[cur].data);
+            System.out.print(' ');
+            cur=(cur+1)%items.length;
+        }
+    }
+    public static void main(String args[]){
+        
+        ArrayDeque L=new ArrayDeque();
+        L.addLast(0);
+        System.out.print(L.size());
+        L.addLast(2);
+        L.addFirst(3);
+        L.addLast(4);
+        L.addLast(5);
+        System.out.print(L.size());
+        L.addFirst(7);
+    }
+
+
 }
